e85ae02b4be2316b411b3bdea4f12c39ea4ab288
Ot Roca <otrocadev@gmail.com>
2025-07-01 10:38:13 +0200

    Implement feature sections with interactive buttons using JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log('patata')
+const simpleBookmarkingBtn = document.getElementById('simple-bookmatking-btn')
+const speedySearchingBtn = document.getElementById('speedy-searching-btn')
+const easySharingBtn = document.getElementById('easy-sharing-btn')
+
+const simpleBookmarking = document.getElementById('simple-bookmatking')
+const speedySearching = document.getElementById('speedy-searching')
+const easySharing = document.getElementById('easy-sharing')
+
+const showSelectedMenuOption = (menuOption) => {
+  simpleBookmarkingBtn
+    .getElementsByTagName('span')[0]
+    .classList.remove('selected')
+  speedySearchingBtn
+    .getElementsByTagName('span')[0]
+    .classList.remove('selected')
+  easySharingBtn.getElementsByTagName('span')[0].classList.remove('selected')
+
+  menuOption.getElementsByTagName('span')[0].classList.add('selected')
+}
+
+const showSelectedContent = (option) => {
+  simpleBookmarking.classList.remove('selected')
+  speedySearching.classList.remove('selected')
+  easySharing.classList.remove('selected')
+
+  option.className = 'selected'
+}
+
+const toggleMenuElement = (option, menuOption) => {
+  if (option.classList.contains('selected')) return 0
+  if (!option.classList.contains('selected')) {
+    showSelectedContent(option)
+    showSelectedMenuOption(menuOption)
+  }
+  return 0
+}
+
+simpleBookmarkingBtn.addEventListener('click', (e) => {
+  toggleMenuElement(simpleBookmarking, simpleBookmarkingBtn)
+})
+
+speedySearchingBtn.addEventListener('click', (e) => {
+  toggleMenuElement(speedySearching, speedySearchingBtn)
+})
+
+easySharingBtn.addEventListener('click', (e) => {
+  toggleMenuElement(easySharing, easySharingBtn)
+})
